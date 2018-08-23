d9d678df35753b3822cb62c9716d6d55083d1092
elemiliman <elemiliman@gmail.com>
2018-08-22 22:58:26 -0400

    Microservice example code in service discovery architecture

---
+++ src/main/java/com/recbuddy/discovery/microservice/RecbuddyEurekaDiscoveryMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.recbuddy.discovery.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RecbuddyEurekaDiscoveryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecbuddyEurekaDiscoveryMicroserviceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/recbuddy/discovery/microservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.recbuddy.discovery.microservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RecbuddyEurekaDiscoveryMicroserviceApplication.class);
+	}
+
+}
